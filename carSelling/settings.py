e6e6c17ec3258cec7b5dc8795c671ccff2cf606b
DawidWIlk81 <38471575+DawidWilk81@users.noreply.github.com>
2021-07-20 16:34:04 +0200

    whole

--- carSelling/settings.py
+++ carSelling/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import datetime
 import os
+import django-heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,5 +134,5 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
